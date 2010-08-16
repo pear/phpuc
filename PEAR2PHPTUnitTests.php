11bd32379f011f72e5c56532f2dd22a8ac05028f
Daniel O'Connor <clockwerx@php.net>
2010-08-16 09:21:50 +0000

    Refactor to strategies
    git-svn-id: http://svn.php.net/repository/pear/ci/phpuc/trunk@302324 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ PEAR2PHPTUnitTests.php
@@ -0,0 +1,35 @@
+<?php
+class PEAR2PHPTUnitTests {
+    public function generate_build(Package $p) {
+        ob_start();
+        ?>
+<?xml version="1.0" encoding="UTF-8"?>
+<project name="<?php print $p->package; ?>" basedir="<?php print $p->source; ?>/<?php print $p->package; ?>" default="build">
+     <target name="checkout">
+        <exec executable="svn" dir="${basedir}">
+            <arg line="up" />
+        </exec>
+    </target>
+    <target name="php-codesniffer">
+        <exec executable="phpcs" dir="${basedir}" output="<?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/logs/checkstyle.xml">
+            <arg line="--report=checkstyle --standard=PEAR <?php print $p->source; ?>/<?php print $p->package; ?>"/>
+        </exec>
+    </target>
+
+    <target name="phpmd">
+        <exec executable="phpmd" dir="${basedir}">
+            <arg line="<?php print $p->source; ?>/<?php print $p->package; ?> xml codesize,unusedcode,naming"/>
+        </exec>
+    </target>
+
+    <target name="phpunit">
+    <exec executable="phpunit" dir="${basedir}" failonerror="on">
+        <arg line="--log-junit <?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/logs/junit.xml tests/AllTests.php" /> 
+    </exec>
+    </target>
+    <target name="build" depends="checkout,php-codesniffer,phpmd,phpunit" />
+</project>
+        <?php
+        return ob_get_clean();
+    }
+}
