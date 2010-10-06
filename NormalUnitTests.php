a61dfba9693214562e8c541895347036546bfe2f
Daniel O'Connor <clockwerx@php.net>
2010-10-06 12:59:50 +0000

    Try different error levels
    git-svn-id: http://svn.php.net/repository/pear/ci/phpuc/trunk@304155 c90b9560-bf6c-de11-be94-00142212c4b1

--- NormalUnitTests.php
+++ NormalUnitTests.php
@@ -31,7 +31,7 @@ class NormalUnitTests {
     <target name="phpunit">
         <exec executable="phpunit" dir="${basedir}" failonerror="on">
             <?php if (extension_loaded('xdebug')) { ?>
-                <arg line="--log-junit <?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/logs/junit.xml 
+                <arg line="-d display_errors='E_ALL & ~E_STRICT' --log-junit <?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/logs/junit.xml 
                             <?php print $this->getBootstrap($p); ?>
                             --coverage-xml <?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/logs/phpunit.coverage.xml
                             --coverage-html <?php print $p->cruisecontrol; ?>/projects/<?php print $p->package; ?>/build/coverage 
