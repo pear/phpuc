627d24b287be23a017b2bcaed377237266d7e7ab
Daniel Convissor <danielc@analysisandsolutions.com>
2011-11-06 12:42:53 -0500

    Put packages-all in phpuc directory (contents will be ignored), so phpuc can be installed anywhere and can be kept up to date more easily.

--- run-tests.sh
+++ run-tests.sh
@@ -1,11 +1,13 @@
 #/bin/sh
 
 # Look ma, SVN update!
-cd ~/packages-all/
-/usr/bin/svn up ~/packages-all
+/usr/bin/svn up packages-all
 
 # And the unit tests runneth
 /usr/local/bin/php tests.php
 
 # http://github.com/smalyshev/migrate/blob/master/migrate.php
-/usr/local/bin/php ~/migrate.php . > unit-test-results/deprecated.txt
+/usr/local/bin/php migrate.php packages-all > packages-all/unit-test-results/deprecated.txt
+
+# Ensure the test results home page is up to date
+cp unit-test-results/index.html packages-all/unit-test-results
