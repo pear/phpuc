13f838e90c92d4cadbfe470944514a3ec2745fe7
Daniel Convissor <danielc@analysisandsolutions.com>
2011-11-12 09:09:43 -0500

    Try locating php executable first.

--- run-tests.sh
+++ run-tests.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+if [ -x /usr/local/bin/php ] ; then
+    php=/usr/local/bin/php
+elif [ -x /usr/bin/php ] ; then
+    php=/usr/bin/php
+else
+    echo "No php executable found."
+    exit 1
+fi
+
 # Ensure the test results home page is up to date
 cp unit-test-results/index.html packages-all/unit-test-results
 
@@ -7,7 +16,7 @@ cp unit-test-results/index.html packages-all/unit-test-results
 /usr/bin/svn up packages-all
 
 # And the unit tests runneth
-/usr/local/bin/php tests.php
+$php tests.php
 
 # http://github.com/smalyshev/migrate/blob/master/migrate.php
-/usr/local/bin/php migrate.php packages-all > packages-all/unit-test-results/deprecated.txt
+$php migrate.php packages-all > packages-all/unit-test-results/deprecated.txt
